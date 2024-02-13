3d50499e80baff93ab4cc31c238d2c5955046d01
valeriiriabinkin <HomaKrut21@mail.ru>
2024-02-13 21:22:14 +0300

    comit another file

--- seminar 1/03_random_action_wrapper.py
+++ seminar 1/03_random_action_wrapper.py
@@ -6,14 +6,15 @@ Action = TypeVar('Action')
 
 
 class RandomActionWrapper(gym.ActionWrapper):
-    def __init__(self, env, epsilon=0.1):
+    def __init__(self, env, epsilon=0.3):
         super(RandomActionWrapper, self).__init__(env)
         self.epsilon = epsilon
 
     def action(self, action: Action) -> Action:
         if random.random() < self.epsilon:
-            print("Random!")
-            return self.env.action_space.sample()
+            print("Right")
+            return 1
+        print("Left")
         return action
 
 

0671f455846e8b13d6b121992e3660c345bff5c8
valeriiriabinkin <HomaKrut21@mail.ru>
2024-03-27 18:58:30 +0300

    comit new files to seminar_5

---
+++ seminar_6/lib/__init__.py
@@ -0,0 +1,10 @@
+"""PTAN stub"""
+
+from . import common
+from . import actions
+from . import experience
+from . import agent
+from . import wrappers
+from . import utils
+
+__all__ = ['common', 'actions', 'experience', 'agent', 'wrappers', 'utils']
